4fbfedcbe5570dadc71a30528adaf20f95779c9b
Giuseppe Maddiona <giuseppe.maddiona@gmail.com>
2022-02-10 19:36:36 +0100

    added logic xml to employee order json

--- xml_parsing.py
+++ xml_parsing.py
@@ -1,12 +1,25 @@
+from asyncore import read
+from typing import Dict, List, Type
 import xml.etree.ElementTree as ET
+import json
 from xmlrpc.client import Boolean
 
 # This class will be moved in another file
 
+class Menu():
 
+    def __init__(self, menu: Dict):
+        self.menu = json.loads(menu)
+
+    def find_dish_id(self, name: str):
+        dishes_list = self.menu['dishes']
+        for el in dishes_list:
+            if el['name'].strip() == name.strip():
+                return el['id']
+        return None
 class EmployeeOrder():
 
-    def __init__(self, name: str, address: dict, is_attending: Boolean, order: str) -> None:
+    def __init__(self, name: str, address: Dict, is_attending: bool, order: str) -> None:
         self.name = name
         self.address = address
         self.is_attending = is_attending
@@ -15,6 +28,25 @@ class EmployeeOrder():
     def __str__(self) -> str:
         return f'{self.name} - {self.address} - {self.is_attending} - {self.order}'
 
+    def __get_dishes(self, menu: Type[Menu]) -> Dict:
+        str_ordered = self.order
+        list_orders = [el.strip() for el in str_ordered.split(',')]
+        orders_dict = {}
+        for el in list_orders:
+            n = el[0]
+            text_order = el[3::].strip()
+            orders_dict[text_order] = n
+        return orders_dict
+
+    def get_json_order(self) -> Dict:
+        customer = {'full_name': self.name, 'address': self.address}
+        employee_json = {'customer': customer, 'dishes': ''}
+        employee_dishes = self.__get_dishes()
+        return employee_dishes
+
+
+
+
 
 def read_file(file: str = 'examples/employee_orders.xml') -> str:
     with open(file, 'r') as file:
@@ -22,7 +54,7 @@ def read_file(file: str = 'examples/employee_orders.xml') -> str:
     return data
 
 
-def employees_orders(xml_file: str) -> list:
+def get_employees_orders(xml_file: str) -> List[EmployeeOrder]:
     root = ET.fromstring(xml_file)
     order_list = []
 
@@ -54,6 +86,8 @@ def employees_orders(xml_file: str) -> list:
 
 if __name__ == "__main__":
     xml_orders = read_file()
-    l = employees_orders(xml_orders)
+    menu_json = read_file('examples/menu.json')
+    menu = Menu(menu_json)
+    l = get_employees_orders(xml_orders)
     for el in l:
-        print(el)
+        print(el.get_json_order())
